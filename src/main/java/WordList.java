9d231a2ddb0fa49cc2aa0f53c015cb88b59f8746
PaperTop <135560395+PaperTop@users.noreply.github.com>
2024-04-03 09:44:47 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,24 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int count = 0;
+    for (int i = 0; i <myList.size(); i++){
+      if (myList.get(i).length() == len){
+        count++;
+      }
+    }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+    ArrayList <string> ans = new ArrayList <string>();
+    int index = 0;
+    for (int i = 0; i < myList.size(); i++){
+      if (myList.get(i).length() != len){
+        ans.add(myList.get(i));
+      }
   }
+  myList = ans;
 }
